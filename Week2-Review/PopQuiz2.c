76bc63bb13e0714b4be37ccf6d228cabea3b8609
gibgibbb <diazkristian321@gmail.com>
2025-06-25 19:02:54 +0800

    Answer to the Pop Quiz earlier. AAAAAA BUGO KAY KO KA ANSWER RA UNTA KO

---
+++ Week2-Review/PopQuiz2.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//06/25/25 Quiz 
+
+typedef struct {
+    char FName[24];
+    char LName[16];
+    char MI;
+}nameType;
+
+typedef struct {
+    nameType name;
+    unsigned int id;
+    char course[9];
+    int year;
+}studRec;
+
+typedef struct node {
+    studRec data;
+    struct node *link;
+}*nodePtr;
+
+typedef struct {
+    nodePtr P;
+    int cnt;
+}LIST;
+
+void init(LIST *L);
+void insertFirst(LIST *L, studRec value);
+void display(LIST L);
+studRec deleteLast(LIST *L);
+void displayStudDel(studRec value);
+
+void displayStudDel(studRec value){
+	printf("%10d %10s %10d", value.id, value.course, value.year);
+}
+
+studRec deleteLast(LIST *L){
+    nodePtr *trav, temp;
+    studRec dummy = {{"XXX", "XXX", 'X'}, 0, "XXX", 0};
+    for(trav = &L->P; *trav != NULL; trav = &(*trav)->link){}
+    if(*trav != NULL){
+        dummy = (*trav)->data;
+        temp = *trav;
+        *trav = (*trav)->link;
+        free(temp);
+    }
+    return dummy;
+}
+
+void display(LIST L){
+    for(; L.P != NULL; L.P = L.P->link){
+        printf("%10d %10s %10d\n", L.P->data.id, L.P->data.course, L.P->data.year);
+    }    
+}
+
+void insertFirst(LIST *L, studRec value){
+    nodePtr temp = malloc(sizeof(struct node));
+    if(temp != NULL){
+        temp->data = value;
+        temp->link = L->P;
+        L->P = temp;
+    }
+}
+
+void init(LIST *L){
+    L->P = NULL;
+    L->cnt = 0;
+}
+
+int main(){
+    
+    LIST L;
+    studRec studA = {{"Kristian", "Diaz", 'W'}, 2210, "BSCS", 3};
+    studRec studB = {{"Work", "Pls", 'A'}, 2223, "BSIT", 2};
+    studRec studC = {{"AAAA", "RAH", 'D'}, 2200, "BSIS", 4};
+    
+    init(&L);
+    insertFirst(&L, studA);
+    insertFirst(&L, studB);
+    insertFirst(&L, studC);
+	printf("\n");
+    studRec studDel = deleteLast(&L);
+    displayStudDel(studDel);
+    
+    return 0;
+}
