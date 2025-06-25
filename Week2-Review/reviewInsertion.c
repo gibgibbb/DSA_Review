031067896b929e4dfa3d3690b0c0f4c48ba4202d
gibgibbb <diazkristian321@gmail.com>
2025-06-25 22:13:44 +0800

    Reviewer for insertAt

---
+++ Week2-Review/reviewInsertion.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define MAX 10
+
+typedef struct {
+	char elem[MAX];
+	int count;
+}LIST;
+
+void init(LIST *L);
+void insertFirst(LIST *L, char val);
+void display(LIST L);
+void insertAt(LIST *L, char val, int pos);
+
+void insertAt(LIST *L, char val, int pos){
+	int x;
+	if(L->count < MAX){
+		if(pos < L->count){
+			for(x = L->count; x > pos; x--){
+				L->elem[x] = L->elem[x - 1];
+			}
+			L->elem[pos] = val;
+			L->count++;
+		}
+	}
+}
+
+void display(LIST L){
+	int x;
+	for(x = 0; x < L.count; x++){
+		printf("%c ", L.elem[x]);
+	}
+}
+
+void insertFirst(LIST *L, char val){
+	int x;
+	if(L->count < MAX){
+		for(x = L->count; x > 0; x--){
+			L->elem[x] = L->elem[x - 1];
+		}
+		L->elem[0] = val;
+		L->count++;
+	}
+}
+
+void init(LIST *L){
+	L->count = 0;
+}
+
+int main(){
+	
+	LIST A;
+	
+	init(&A);
+	insertFirst(&A, 'c');
+	insertFirst(&A, 's');
+	insertFirst(&A, 'u');
+	insertAt(&A, 'd', 0);
+	display(A);
+	
+	return 0;
+}
