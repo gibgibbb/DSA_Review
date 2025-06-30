09b29756734efe335b3674f3efb1cbf80aada723
gibgibbb <diazkristian321@gmail.com>
2025-06-30 21:42:57 +0800

    Added the function pop, push, init, top, display

--- Week2-Review/Stack.c
+++ Week2-Review/Stack.c
@@ -7,6 +7,38 @@ typedef struct {
 }Stack;
 
 void initStack(Stack *S);
+void push(Stack *S, char val);
+char top(Stack S);
+void pop(Stack *S);
+void display(Stack S);
+
+char top(Stack S){
+	return (S.top != -1)? S.elem[S.top]: '\0';
+}
+
+void display(Stack S){
+	Stack temp;
+	initStack(&temp);
+	
+	for(; S.top != -1; pop(&S)){
+		char c = S.elem[S.top];
+		push(&temp, c);
+		printf("%c ", c);
+	}
+}
+
+void push (Stack *S, char val){
+	if(S->top != MAX - 1){
+		S->elem[++S->top] = val;
+	}
+}
+
+void pop(Stack *S){
+	if(S->top != -1){
+		S->top--;
+	}
+}
+
 
 void initStack(Stack *S){
 	S->top = -1;
@@ -15,8 +47,10 @@ void initStack(Stack *S){
 int main(){
 	
 	Stack A;
-	
 	initStack(&A);
-	
+	push(&A, 'a');
+	push(&A, 'b');
+	push(&A, 'c');
+	display(A);
 	return 0;
 }
