87a7faa78a14e2f693dbc284a41659d7c8421a74
gibgibbb <diazkristian321@gmail.com>
2025-06-30 23:30:53 +0800

    Added the pop quiz earlier about getLastElem in VHeap

---
+++ Week2-Review/PopQuiz3CBase.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#define MAX 3
+
+typedef struct {
+	char FName[24];
+	char LName[16];
+	char Mi;
+}Nametype;
+
+typedef struct {
+	Nametype data;
+	int link;
+}Nodetype;
+
+typedef struct {
+	Nodetype Nodes[MAX];
+	int avail;
+}VirtualHeap;
+
+typedef int CList;
+
+void initVheap(VirtualHeap *VH);
+void initList(CList *L);
+int allocSpace(VirtualHeap *VH);
+void deallocSpace(VirtualHeap *VH, int ndx);
+void insertFirst(VirtualHeap *VH, CList *L, Nametype val);
+void display(VirtualHeap VH, CList L);
+Nametype getLastElem(VirtualHeap VH, CList L); // gets the last elem of the VHeap and return the data to the calling function, if it exists. If it doesn't exist, return a dummy data;
+
+Nametype getLastElem(VirtualHeap VH, CList L){
+	Nametype dummy = {"xxx", "xxx", 'x'};
+	CList trav = L;
+	
+	if(L != -1){
+		for(trav = L; VH.Nodes[trav].link != -1; trav = VH.Nodes[trav].link){}
+		dummy = VH.Nodes[trav].data;
+	}
+	return dummy;
+	
+	/* just choose what u prefer and what u understand the most
+	if(L != -1){
+		for(; VH.Nodes[trav].link != -1; trav = VH.Nodes[trav].link){}
+	}
+	return (L != -1)? VH.Nodes[trav].data : dummy;
+	*/
+}
+
+void display(VirtualHeap VH, CList L){
+	int trav;
+	for(trav = L; trav != -1; trav = VH.Nodes[trav].link){
+		printf("%s\n", VH.Nodes[trav].data.FName);
+	}
+}
+
+void insertFirst(VirtualHeap *VH, CList *L, Nametype val){
+	int temp = allocSpace(VH);
+	if(temp != -1){
+		VH->Nodes[temp].data = val;
+		VH->Nodes[temp].link = *L;
+		*L = temp;
+	}
+}
+
+int allocSpace(VirtualHeap *VH){
+	int temp = VH->avail;
+	if(temp != -1){
+		VH->avail = VH->Nodes[temp].link;
+	}
+	return temp;
+}
+
+void deallocSpace(VirtualHeap *VH, int ndx){
+	VH->Nodes[ndx].link = VH->avail;
+	VH->avail = ndx;
+}
+
+void initList(CList *L){
+	*L = -1;
+}
+
+void initVheap(VirtualHeap *VH){
+	VH->avail = MAX - 1;
+	int x;
+	for(x = VH->avail; x >= 0; x--){
+		VH->Nodes[x].link = x - 1;
+	}
+}
+
+int main(){
+	
+	VirtualHeap A;
+	CList CL;
+	
+	initList(&CL);
+	initVheap(&A);
+	
+	Nametype studA = {"Kristian", "Diaz", 'W'};
+    Nametype studB = {"Work", "Pls", 'A'};
+    Nametype studC = {"AAAA", "RAH", 'D'};
+    
+    insertFirst(&A, &CL, studA);
+	insertFirst(&A, &CL, studB);
+	insertFirst(&A, &CL, studC);
+	
+	display(A, CL);
+	
+	Nametype catcher = getLastElem(A, CL);
+	printf("\nJust to check if it works\n%s", catcher.FName);
+	
+	return 0;
+}
