842803e7be0619728a885a28c1ebd582e835540a
gibgibbb <diazkristian321@gmail.com>
2025-07-01 23:42:57 +0800

    Added the enqueue function

--- Week2-Review/Queues.c
+++ Week2-Review/Queues.c
@@ -9,6 +9,15 @@ typedef struct {
 }Queue;
 
 void initQueue(Queue *Q);
+void enqueue(Queue *Q, char x);
+void dequeue(Queue *Q, char x);
+
+void enqueue(Queue *Q, char x){
+	if((Q->rear + 2) % MAX != Q->front){
+		Q->rear = (Q->rear + 1) % MAX;
+		Q->elem[Q->rear] = x;
+	}
+}
 
 void initQueue(Queue *Q){
 	Q->front = 0;
@@ -20,6 +29,8 @@ int main (){
 	Queue A;
 	
 	initQueue(&A);
+	enqueue(&A, 'a');
+	enqueue(&A, 'b');
 	
 	return 0;
 }
