de2a0a21c8a9ec09dff0135c87c3199a8f6ae79e
gibgibbb <diazkristian321@gmail.com>
2025-07-02 21:08:03 +0800

    Created dequeue and display function for the array implementation of Queues

--- Week2-Review/Queues.c
+++ Week2-Review/Queues.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAX 8
 
-// Queues 30/06/25
+// Queues 30/06/25 Circular Array
 
 typedef struct {
 	char elem[MAX];
@@ -10,12 +10,27 @@ typedef struct {
 
 void initQueue(Queue *Q);
 void enqueue(Queue *Q, char x);
-void dequeue(Queue *Q, char x);
+void dequeue(Queue *Q);
+void display(Queue Q);
+
+void display(Queue Q){
+	int x;
+	for(x = (Q.rear + 1) % MAX; x != Q.front; dequeue(&Q)){
+		enqueue(&Q, Q.elem[Q.front]);
+		printf("%c ", Q.elem[Q.front]);
+	}
+}
 
 void enqueue(Queue *Q, char x){
 	if((Q->rear + 2) % MAX != Q->front){
 		Q->rear = (Q->rear + 1) % MAX;
 		Q->elem[Q->rear] = x;
+	}
+}
+
+void dequeue(Queue *Q){
+	if((Q->rear + 1) % MAX != Q->front){
+		Q->front = (Q->front + 1) % MAX;
 	}
 }
 
@@ -31,6 +46,11 @@ int main (){
 	initQueue(&A);
 	enqueue(&A, 'a');
 	enqueue(&A, 'b');
+	enqueue(&A, 'c');
+	display(A);
+	printf("\n");
+	dequeue(&A);
+	display(A);
 	
 	return 0;
 }
