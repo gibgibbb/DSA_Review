853142354545f1f8bb5e415664769efbd666854b
gibgibbb <diazkristian321@gmail.com>
2025-07-15 21:13:54 +0800

    Added insertion to POT function

--- Week4-Review/PriorityQueue.c
+++ Week4-Review/PriorityQueue.c
@@ -6,6 +6,21 @@ typedef struct {
 	int lastNdx;
 }PQ;
 
+void init(PQ *pq);
+void insertToPOT(PQ *pq, int val);
+
+//MAXHEAP val > parentElem (parent should be larger than children)
+void insertToPOT(PQ *pq, int val){
+	if(pq->lastNdx < MAX - 1){
+		pq->lastNdx++;
+		int x, parentNdx;
+		for(x = pq->lastNdx, parentNdx = (x - 1)/2; x > 0 && val > pq->elem[parentNdx]; x = parentNdx, parentNdx = (parentNdx - 1)/2){
+			pq->elem[x] = pq->elem[parentNdx];
+		}
+		pq->elem[x] = val;
+	}
+}
+
 void init(PQ *pq){
 	pq->lastNdx = -1;
 }
@@ -15,6 +30,10 @@ int main(){
 	
 	PQ A;
 	init(&A);
+	insertToPOT(&A, 5);
+	insertToPOT(&A, 6);
+	insertToPOT(&A, 15);
+	insertToPOT(&A, 7);
 
 	return 0;
 }
