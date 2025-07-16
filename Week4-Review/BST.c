a2fe54fd0fc4cf628488c06a2fc40f64c9da182b
gibgibbb <diazkristian321@gmail.com>
2025-07-16 23:18:22 +0800

    Added delete and insert function in BST

--- Week4-Review/BST.c
+++ Week4-Review/BST.c
@@ -7,6 +7,52 @@ typedef struct node {
 	struct node *left, *right;
 }*nodePtr;
 
+void init(nodePtr *N);
+void insert(nodePtr *N, int val);
+void delete(nodePtr *N, int val);
+void display(nodePtr N);
+
+void display(nodePtr N){
+	if(N != NULL){
+		printf("%d ", N->elem);
+		display(N->left);
+		display(N->right);
+	}
+}
+
+void insert(nodePtr *N, int val){
+	nodePtr *trav, temp;
+	for(trav = N; *trav != NULL && (*trav)->elem != val;){
+		trav = (val < (*trav)->elem) ? &(*trav)->left : &(*trav)->right;
+	}
+	if(*trav == NULL){
+		temp = malloc(sizeof(struct node));
+		temp->elem = val;
+		temp->left = temp->right = NULL;
+		*trav = temp;
+	}
+}
+
+void delete(nodePtr *N, int val){
+	nodePtr *trav, temp;
+	for(trav = N; *trav != NULL && (*trav)->elem != val;){
+		trav = (val < (*trav)->elem) ? &(*trav)->left : &(*trav)->right;
+	}
+	if(*trav != NULL){
+		temp = *trav;
+		if(temp->left != NULL && temp->right != NULL){
+			nodePtr *trav2;
+			for(trav2 = &temp->left; *trav != NULL && (*trav2)->right != NULL; trav2 = &(*trav2)->right){}
+			temp->elem = (*trav2)->elem;
+			temp = *trav2;
+			*trav2 = temp->left;
+		} else {
+			*trav = (temp->left != NULL) ? temp->left : temp->right;
+		}
+		free(temp);
+	}
+}
+
 void init(nodePtr *N){
 	*N = NULL;
 }
@@ -15,6 +61,13 @@ int main(){
 	
 	nodePtr A;
 	init(&A);
+	insert(&A, 5);
+	insert(&A, 10);
+	insert(&A, 3);
+	display(A);
+	delete(&A, 10);
+	printf("\ndelete\n");
+	display(A);
 	
 	return 0;
 }
